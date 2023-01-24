05ce8fd25b4edd40970f767ff4f5b8f73112f1f2
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-01-24 21:35:48 +0530

    portfolio: Add Scroll to Top behaviour

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,7 +3,7 @@
 </script> 
 <main>
     <div class="flex w-screen fixed top-0 left-0 justify-between px-24 py-8 items-center" style="backdrop-filter: blur(10px)">
-        <div class="font-bold text-primary-dark text-4xl">Ravishanker</div>
+        <div use:scrollto={'top'} class="font-bold text-primary-dark text-4xl">Ravishanker</div>
         <div class="flex justify-evenly font-semibold items-center gap-16">
             <a use:scrollto={'#about'} class="header">About</a>
             <a use:scrollto={'#portfolio'} class="header">Portfolio</a>
