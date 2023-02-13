363bc50de610928c68af1e4c06058a75e55a9dd9
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-02-13 19:25:08 +0530

    Portfolio: Switch to Font Awesome CDN

--- src/App.svelte
+++ src/App.svelte
@@ -8,8 +8,8 @@
 
 <main>
   <link rel="stylesheet" href="./smui.css" />
-  <link href="./../node_modules/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
-  <div class="absolute z-10">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" integrity="sha512-F5QTlBqZlvuBEs9LQPqc1iZv2UMxcVXezbHzomzS6Df4MZMClge/8+gXrKw2fl5ysdk4rWjR0vKS7NNkfymaBQ==" crossorigin="anonymous"></script>  <div class="absolute z-10">
     <Header />
   </div>
   <About />
