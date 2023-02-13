4cd7c46a9c9343c790360b3e0055d492992ea169
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-02-13 19:44:06 +0530

    Portfolio: Tailwind: Update Tailwind Config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
     colors: {
-      'black': '17171C',
-      'light-white': '#E7E7FC',
-      'dark-white': '#DFDFFB',
-      'primary-dark': '#6767F4',
-      'primary-light': '#9B9BF8',
-    }
+      black: "17171C",
+      "card-black": "#1F1F2B",
+      "light-white": "#E7E7FC",
+      "dark-white": "#DFDFFB",
+      "primary-dark": "#6767F4",
+      "primary-light": "#9B9BF8",
+    },
   },
   plugins: [],
-}
+};
